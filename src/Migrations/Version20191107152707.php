1e9d6ff83bd86b7742678f6f978ea720e8446d59
alaasho <alaashokri93@gmail.com>
2019-11-07 17:11:09 +0100

    first update

---
+++ src/Migrations/Version20191107152707.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20191107152707 extends AbstractMigration
+{
+    public function getDescription() : string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema) : void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('CREATE TABLE ticket (id INT AUTO_INCREMENT NOT NULL, text_body VARCHAR(255) NOT NULL, is_public TINYINT(1) NOT NULL, is_open TINYINT(1) NOT NULL, is_in_progress TINYINT(1) NOT NULL, customer_coment VARCHAR(255) NOT NULL, agent_coment VARCHAR(255) NOT NULL, is_waiting_for_feedback TINYINT(1) NOT NULL, is_escalated TINYINT(1) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+    }
+
+    public function down(Schema $schema) : void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('DROP TABLE ticket');
+    }
+}

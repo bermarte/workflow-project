6ebeee775fe740177215704abe44ef3452ad5c0a
bermarte <bermarte@hotmail.com>
2019-11-13 18:47:07 +0100

    get current user for the ticket form

--- src/Form/TicketType.php
+++ src/Form/TicketType.php
@@ -6,19 +6,34 @@ use App\Entity\Ticket;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use App\Entity\User;
+use Symfony\Component\Security\Core\Security;
+
+
+
 
 class TicketType extends AbstractType
 {
+    //get the current user
+    private $security;
+
+    public function __construct(Security $security)
+    {
+        $this->security = $security;
+    }
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
+        //current user
+        $user = $this->security->getUser();
+
         $builder
             ->add('text_body')
             ->add('customer_comment')
-            ->add('user_id')
+
             ->add('is_open')
-
-
+            ->add('user_id')
              /*
+              * ->add('user_id')
              ->add('is_public')
              ->add('is_in_progress')
             ->add('agent_comment')

054edb60e89bea767db55adb75903747d35645de
bermarte <bermarte@hotmail.com>
2019-11-14 00:12:27 +0100

    more tweak on the tickets form

--- src/Form/TicketType.php
+++ src/Form/TicketType.php
@@ -3,11 +3,12 @@
 namespace App\Form;
 
 use App\Entity\Ticket;
+use phpDocumentor\Reflection\Types\Integer;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
-use App\Entity\User;
 use Symfony\Component\Security\Core\Security;
+use Symfony\Component\Form\Extension\Core\Type\HiddenType;
 
 
 
@@ -29,17 +30,19 @@ class TicketType extends AbstractType
         $builder
             ->add('text_body')
             ->add('customer_comment')
-
             ->add('is_open')
-            ->add('user_id')
+            ->add('user_id', HiddenType::class, [
+                'data' => $user,
+            ]);
+
              /*
-              * ->add('user_id')
+             ->add('user_id')
              ->add('is_public')
              ->add('is_in_progress')
-            ->add('agent_comment')
-            ->add('is_waiting_for_feedback')
-            ->add('is_escalated')
-            ->add('relation')
+             ->add('agent_comment')
+             ->add('is_waiting_for_feedback')
+             ->add('is_escalated')
+             ->add('relation')
              */
         ;
     }
